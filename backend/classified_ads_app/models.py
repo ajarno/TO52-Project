92dfaaff8cfc2aaa72864ac9d2910be1197ba2da
kAgotsi <cyrilleagotsi@gmail.com>
2020-10-12 22:54:20 +0200

    Add Discussion model.

--- backend/classified_ads_app/models.py
+++ backend/classified_ads_app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+# Define discussion model
+
+class Discussion(models.Model):
+    # Foreign keys 
+    sender = models.ForeignKey(UserAccount,on_delete=models.CASCADE, related_name='discussion')
+    receiver = models.ForeignKey(UserAccount,on_delete=models.CASCADE, related_name='discussion')
+    related_ad = models.ForeignKey(Ad,on_delete=models.CASCADE, related_name='ad')
+   
+    # Inheritance
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+
+    # Attributes
+    created_at = models.DateField(auto_now_add=True)
+    content = models.TextField(max_length=1500,blank=False)
