7dd49c753c8d617b401b31aa7eeeac86f140d4ba
kAgotsi <cyrilleagotsi@gmail.com>
2020-10-14 17:12:03 +0200

    Register UserProfile , User and discussion models.

--- backend/classified_ads_app/admin.py
+++ backend/classified_ads_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import (
+    Discussion, User,UserProfile
+)
 
 # Register your models here.
+admin.site.register(Discussion)
+admin.site.register(UserProfile)
--- backend/config/settings.py
+++ backend/config/settings.py
@@ -37,8 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework'
+    'api'
     'classified_ads_app',
 ]
+
+AUTH_USER_MODEL = 'classified_ads_app.User'
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
