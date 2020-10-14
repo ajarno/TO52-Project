b45b34f0b6818cfa27707ddb7ed2bb505a1eae0c
kAgotsi <cyrilleagotsi@gmail.com>
2020-10-14 17:14:46 +0200

    Add user list , register, profile list view .

--- backend/classified_ads_app/views.py
+++ backend/classified_ads_app/views.py
@@ -1,3 +1,55 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.response import Response
+from knox.models import AuthToken
+from .models import Discussion, User, UserProfile
+from .serializers import ( DiscussionSerializer, 
+UserSerializer, UserProfileSerializer,RegisterSerializer)
+from rest_framework.generics import (ListCreateAPIView,
+RetrieveUpdateDestroyAPIView,)
+from rest_framework import generics, permissions
+from .permissions import IsOwnerProfileOrReadOnly
 
-# Create your views here.
+
+
+# User ViewSet
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = (AllowAny,)
+
+# Register API
+class RegisterView(generics.GenericAPIView):
+    serializer_class = RegisterSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        return Response({
+        "user": UserSerializer(user, context=self.get_serializer_context()).data,
+        "token": AuthToken.objects.create(user)[1]
+        })
+
+
+class UserProfileListCreateView(ListCreateAPIView):
+    queryset=UserProfile.objects.all()
+    serializer_class=UserProfileSerializer
+    permission_classes=[IsAuthenticated]
+
+    def perform_create(self, serializer):
+        user=self.request.user
+        serializer.save(user=user)
+
+
+class userProfileDetailView(RetrieveUpdateDestroyAPIView):
+    queryset=UserProfile.objects.all()
+    serializer_class=UserProfileSerializer
+    permission_classes=[IsOwnerProfileOrReadOnly,IsAuthenticated]
+
+        
+# Discussion ViewSets
+class DiscussionViewSet(viewsets.ModelViewSet):
+    queryset = Discussion.objects.all()
+    serializer_class = DiscussionSerializer
