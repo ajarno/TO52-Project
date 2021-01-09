9e52efd5077db87fa5baa1bc424a563cacbf5495
Anais Jarno <jarno.ana39@gmail.com>
2021-01-09 16:16:38 +0100

    deploy: Update backend for the deployment

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ut-annonces.herokuapp.com']
 
 
 # Application definition
